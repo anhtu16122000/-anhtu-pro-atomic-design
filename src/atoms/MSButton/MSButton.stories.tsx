c432e45228943f1d88a8b89b8e62649588958325
anh tu <anhtu16122000@gmail.com>
2023-12-04 16:20:09 +0700

    add button

--- src/atoms/MSButton/MSButton.stories.tsx
+++ src/atoms/MSButton/MSButton.stories.tsx
@@ -1,5 +1,11 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import MSButton from "./MSButton";
+import { MdSave } from "react-icons/md";
+import { TbDatabaseImport } from "react-icons/tb";
+import { FiPlusCircle } from "react-icons/fi";
+import { FaEdit, FaRegTrashAlt } from "react-icons/fa";
+import { PiMicrosoftExcelLogoFill } from "react-icons/pi";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -11,8 +17,69 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Create: Story = {
   args: {
-    children: "Button",
+    children: "Create",
+    backgroundColor: "#4caf50",
+    color: "white",
+    hoverEffect: "scale-out",
+    size: "md",
+    loading: false,
+    Icon: <FiPlusCircle size={16} />,
+  },
+};
+export const Save: Story = {
+  args: {
+    children: "Save",
+    backgroundColor: "#0989ff",
+    color: "white",
+    hoverEffect: "scale-out",
+    size: "md",
+    loading: false,
+    Icon: <MdSave size={18} />,
+  },
+};
+export const Edit: Story = {
+  args: {
+    children: "Edit",
+    backgroundColor: "#d28923",
+    color: "white",
+    hoverEffect: "scale-out",
+    size: "md",
+    loading: false,
+    Icon: <FaEdit size={17} />,
+  },
+};
+export const Delete: Story = {
+  args: {
+    children: "Delete",
+    backgroundColor: "#ff595e",
+    color: "white",
+    hoverEffect: "scale-out",
+    size: "md",
+    loading: false,
+    Icon: <FaRegTrashAlt size={14} />,
+  },
+};
+export const Import: Story = {
+  args: {
+    children: "Export",
+    backgroundColor: "#f77f35",
+    color: "white",
+    hoverEffect: "scale-out",
+    size: "md",
+    loading: false,
+    Icon: <TbDatabaseImport size={17} />,
+  },
+};
+export const Export: Story = {
+  args: {
+    children: "Export",
+    backgroundColor: "#1d8240",
+    color: "white",
+    hoverEffect: "scale-out",
+    size: "md",
+    loading: false,
+    Icon: <PiMicrosoftExcelLogoFill size={18} />,
   },
 };
--- src/atoms/MSButton/MSButton.tsx
+++ src/atoms/MSButton/MSButton.tsx
@@ -1,11 +1,77 @@
 import React from "react";
+import styles from "./MSButton.module.scss";
+import clsx from "clsx";
 
-export interface MSButtonProps {
-  children: React.ReactNode;
-}
+type MSButtonProps = {
+  loading?: boolean;
+  backgroundColor: string;
+  color: string;
+  Icon?: React.ReactNode;
+  hoverEffect?: "opacity" | "scale-out" | "scale-in";
+  children?: React.ReactNode;
+  size?: "sm" | "md" | "lg";
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const MSButton: React.FC<MSButtonProps> = ({ children }) => {
-  return <button>{children}</button>;
+const LoadingComp: React.FC<{}> = () => (
+  <svg
+    className={clsx(styles.spinner, "text-white w-[15px] h-[15px]")}
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      stroke-width="4"
+    ></circle>
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    />
+  </svg>
+);
+
+const MSButton: React.FC<MSButtonProps> = (props) => {
+  const {
+    Icon,
+    className = "",
+    children,
+    backgroundColor = "#000000",
+    hoverEffect = "opacity",
+    type = "create",
+    disabled = false,
+    loading = false,
+    color = "white",
+    size = "md",
+    ...restProps
+  } = props;
+  let CurrentIcon = Icon && Icon;
+  if (loading) CurrentIcon = <LoadingComp />;
+
+  return (
+    <button
+      className={clsx(
+        styles.root,
+        {
+          [styles.sizeSm]: size === "sm",
+          [styles.sizeMd]: size === "md",
+          [styles.sizeLg]: size === "lg",
+          [styles.hoverOpacity]: hoverEffect === "opacity",
+          [styles.hoverScaleOut]: hoverEffect === "scale-out",
+          [styles.hoverScaleIn]: hoverEffect === "scale-in",
+        },
+        className,
+      )}
+      style={{ backgroundColor, color }}
+      {...restProps}
+    >
+      {CurrentIcon && CurrentIcon}
+      {children}
+    </button>
+  );
 };
 
 export default MSButton;
